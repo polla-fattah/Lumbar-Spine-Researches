f9299b3ec348a07b413c7751a026fb37c167d46e
Polla Fattah <polla.fattah@gmail.com>
2026-08-25 02:53:52 +0300

    refactor(impl): update folder numbering sequence (00_deidentify, 01_prepare, 02_data_manifest, 03_dicom_geometry)

--- implementation/run_env_check.bat
+++ implementation/run_env_check.bat
@@ -1,6 +1,6 @@
 @echo off
 REM AMOG-Net Root Windows Launcher
-cd /d "%~dp000_prepare"
+cd /d "%~dp001_prepare"
 echo Running AMOG-Net Automated Environment Setup and Verification...
 python auto_setup_and_verify.py
 pause
